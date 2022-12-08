8247d17e2c6603cc56deb1e8757799897ea23151
Capucinoxx <bendertermebc@gmail.com>
2022-12-08 12:11:04 -0500

    [feat] - ajout fonction Mul

--- aoc-2022/common/maths.go
+++ aoc-2022/common/maths.go
@@ -8,3 +8,12 @@ func Sum[T int | float64](s []T) T {
 	}
 	return sum
 }
+
+// Mul multiplies all the values in the slice
+func Mul[T int | float64](s []T) T {
+	var mul T = 1
+	for _, v := range s {
+		mul *= v
+	}
+	return mul
+}

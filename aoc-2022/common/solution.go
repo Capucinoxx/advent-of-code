edd42ce7ce53534ccf07e3f0c21a83f021a38941
Capucinoxx <bendertermebc@gmail.com>
2022-12-10 01:19:12 -0500

    [feat] - ajout du support réponse sur plusieurs lignes

--- aoc-2022/common/solution.go
+++ aoc-2022/common/solution.go
@@ -1,6 +1,9 @@
 package common
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Solution interface {
 	// Title returns the title of the solution
@@ -16,10 +19,45 @@ type Solution interface {
 	PartTwo() string
 }
 
+// Print prints the solution
 func Print(s Solution) {
-	fmt.Println(s.Title())
-	fmt.Println("Part One:", s.PartOne())
-	fmt.Println("Part Two:", s.PartTwo())
-	fmt.Println("--------------------")
-	fmt.Println("")
+	var sb strings.Builder
+
+	// title
+	sb.WriteString(s.Title())
+	sb.WriteString("\n")
+
+	// part one
+	AppendPart(&sb, "Part One: ", s.PartOne())
+
+	// part two
+	AppendPart(&sb, "Part Two: ", s.PartTwo())
+
+	// footer
+	sb.WriteString("--------------------\n\n")
+
+	fmt.Print(sb.String())
+}
+
+// AppendPart appends the part to the string builder. If the solution has
+// multiple lines, it will be indented by the length of the part string.
+func AppendPart(sb *strings.Builder, part string, solution string) {
+	lines := strings.Split(solution, "\n")
+	if len(lines) == 1 {
+		sb.WriteString(part)
+		sb.WriteString(solution)
+		sb.WriteString("\n")
+		return
+	}
+
+	sb.WriteString(part)
+	sb.WriteString(lines[0])
+	sb.WriteString("\n")
+
+	space := strings.Repeat(" ", len(part))
+	for _, line := range lines[1:] {
+		sb.WriteString(space)
+		sb.WriteString(line)
+		sb.WriteString("\n")
+	}
 }
